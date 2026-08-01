8a0895452d52919e30e9c87f67ae243411a6cc1d
Akash Kumar <akashkumarakashkumar39006@gmail.com>
2026-08-01 20:10:03 +0530

    Practic of Array

--- Arrays4/Binary_search.java
+++ Arrays4/Binary_search.java
@@ -1,29 +1,52 @@
 public class Binary_search {
-
-    public static int Binary_Search(int nums[],int target){
-        int start =0;
+    public static int Binary_search_decrease(int nums[],int target){
+        int start = 0;
         int end = nums.length-1;
         while(start<=end){
-            // int mid = (i+j)/2;
             int mid = start + (end-start)/2;
             if(nums[mid]==target){
                 return mid;
             }
             else if(nums[mid]<target){
-                start = mid +1;
+                end = mid -1;
             }
             else{
-                end = mid-1;
+                start = mid+1;
             }
-        
         }
         return -1;
+
     }
+
+    // public static int Binary_Search_increase(int nums[],int target){
+    //     int start =0;
+    //     int end = nums.length-1;
+    //     while(start<=end){
+    //         // int mid = (i+j)/2;
+    //         int mid = start + (end-start)/2;
+    //         if(nums[mid]==target){
+    //             return mid;
+    //         }
+    //         else if(nums[mid]<target){
+    //             start = mid +1;
+    //         }
+    //         else{
+    //             end = mid-1;
+    //         }
+        
+    //     }
+    //     return -1;
+    // }
     public static void main(String[] args){
-        int nums[] = {3,4,5,6,76,77,87,97,111};
-        int target = 77;
-        int x = Binary_Search(nums,target);
-        System.out.println(x);
+        // int nums[] = {3,4,5,6,76,77,87,97,111};
+        // int target = 77;
+        // int x = Binary_Search_increase(nums,target);
+        // System.out.println(x);
+
+        int nums[] = {23,21,20,12,11};
+        int target = 23;
+        int v = Binary_search_decrease(nums, target);
+        System.out.println(v);
     }
     
 }
