a4566e5107ab773adf4303501ef234a5de79c7f9
Akash Kumar <akashkumarakashkumar39006@gmail.com>
2026-08-09 14:08:27 +0530

    Update

--- Sorting5/InPlace/insertion.java
+++ Sorting5/InPlace/insertion.java
@@ -1,7 +1,23 @@
 package Sorting5.InPlace;
 
 public class insertion {
+
+    static void sort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int crr =arr[i];
+            int prev = i-1;
+            while(prev>=0 && arr[prev]>crr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = crr;
+        }
+    }
     public static void main(String[] args){
-        
+        int arr[] = {4,5,3,2,1};
+        sort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
     }
 }
